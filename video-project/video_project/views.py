33c1efa4d283018729a01855ef28e051d6a21908
rodrigogomesrc <rocha.rodrigogomes@gmail.com>
2019-06-20 13:27:37 -0300

    template iteration test

--- video-project/video_project/views.py
+++ video-project/video_project/views.py
@@ -1,6 +1,30 @@
 from pyramid.view import view_config
 
 
-@view_config(route_name='home', renderer='templates/mytemplate.jinja2')
-def my_view(request):
+@view_config(route_name='home', renderer='templates/home_content.jinja2')
+def home(request):
     return {'project': 'video-project'}
+
+
+@view_config(route_name='videos', renderer='templates/video_content.jinja2')
+def videos(request):
+
+    videos = {"videos": [
+        {"video_name": "video1", "video_theme": "tema"},
+        {"video_name": "video2", "video_theme": "tema"},
+        {"video_name": "video3", "video_theme": "tema"},
+        {"video_name": "video4", "video_theme": "tema"},
+        {"video_name": "video5", "video_theme": "tema"},
+        {"video_name": "video6", "video_theme": "tema"}
+    ]}
+
+    return videos
+
+
+@view_config(route_name='themes', renderer='templates/themes_content.jinja2')
+def themes(request):
+    return {}
+
+
+
+
