351202f56ed8c3e4e6089c8bfb5fd0d73aedf8e8
rodrigogomesrc <rocha.rodrigogomes@gmail.com>
2019-06-20 19:53:08 -0300

    video score

--- video-project/video_project/views.py
+++ video-project/video_project/views.py
@@ -21,16 +21,28 @@ class Database(object):
 
         return videos
 
+    def _calculate_score(self, name):
+
+        video = self.collection.find_one({"name": name})
+        video_up = video["thumbs_up"]
+        video_down = video["thumbs_down"]
+        score = video_up - (video_down / 2)
+        query1 = {"name": name}
+        query2 = {"$set": {"score": score}}
+        self.collection.update(query1, query2)
+
     def thumbs_up(self, name):
 
         query1 = {"name": name}
         query2 = {"$inc": {"thumbs_up": 1}}
         self.collection.update(query1, query2)
+        self._calculate_score(name)
 
     def thumbs_down(self, name):
         query1 = {"name": name}
         query2 = {"$inc": {"thumbs_down": 1}}
         self.collection.update(query1, query2)
+        self._calculate_score(name)
 
     def upload_video(self, name, theme):
 
@@ -46,6 +58,9 @@ class Database(object):
 
         return {"response": "video created"}
 
+    def create_theme_ranking(self):
+        pass
+
 
 db = Database()
 number = 0
@@ -60,11 +75,9 @@ def home(request):
         name = str(request.POST.get('name'))
 
         if request.POST.get('action') == "1":
-            print("Joinha")
             db.thumbs_up(name)
 
         else:
-            print("Não gostei")
             db.thumbs_down(name)
 
     videos = db.get_videos()
