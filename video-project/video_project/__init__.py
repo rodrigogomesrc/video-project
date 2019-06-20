d24a894660ca554811a716e1459b7de274df6a33
rodrigogomesrc <rocha.rodrigogomes@gmail.com>
2019-06-20 15:35:08 -0300

    get videos from database

--- video-project/video_project/__init__.py
+++ video-project/video_project/__init__.py
@@ -8,7 +8,7 @@ def main(global_config, **settings):
     config.include('pyramid_jinja2')
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
-    config.add_route('videos', '/videos')
     config.add_route('themes', '/themes')
+    config.add_route('create', '/create_video')
     config.scan()
     return config.make_wsgi_app()
--- video-project/video_project/views.py
+++ video-project/video_project/views.py
@@ -1,22 +1,40 @@
 from pyramid.view import view_config
+from pymongo import MongoClient
+
+
+class Database(object):
+
+    def __init__(self):
+
+        self.client = MongoClient()
+        self.db = self.client["videos"]
+        self.collection = self.db["videos"]
+
+    def get_videos(self):
+
+        videos = []
+        for video in self.collection.find():
+            videos.append(video)
+
+        videos = {"videos": videos}
+
+        return videos
+
+    def thumbs_up(self):
+        pass
+
+    def thumbs_down(self):
+        pass
+
+    def upload_video(self, name, theme):
+        pass
 
 
 @view_config(route_name='home', renderer='templates/home_content.jinja2')
 def home(request):
-    return {'project': 'video-project'}
 
-
-@view_config(route_name='videos', renderer='templates/video_content.jinja2')
-def videos(request):
-
-    videos = {"videos": [
-        {"video_name": "video1", "video_theme": "tema"},
-        {"video_name": "video2", "video_theme": "tema"},
-        {"video_name": "video3", "video_theme": "tema"},
-        {"video_name": "video4", "video_theme": "tema"},
-        {"video_name": "video5", "video_theme": "tema"},
-        {"video_name": "video6", "video_theme": "tema"}
-    ]}
+    db = Database()
+    videos = db.get_videos()
 
     return videos
 
@@ -26,5 +44,8 @@ def themes(request):
     return {}
 
 
-
-
+@view_config(route_name='create', renderer='json')
+def create(request):
+    name = str(request.POST.get('name'))
+    theme = str(request.POST.get('theme'))
+    return {"response": "video created"}
