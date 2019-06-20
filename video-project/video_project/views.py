490f3b465f9d01d167a422589efab9dd23951029
rodrigogomesrc <rocha.rodrigogomes@gmail.com>
2019-06-20 18:48:19 -0300

    thumbs buttons

--- video-project/video_project/views.py
+++ video-project/video_project/views.py
@@ -1,4 +1,5 @@
 from pyramid.view import view_config
+from pyramid.httpexceptions import HTTPFound
 from pymongo import MongoClient
 
 
@@ -20,27 +21,59 @@ class Database(object):
 
         return videos
 
-    def thumbs_up(self):
-        pass
+    def thumbs_up(self, name):
 
-    def thumbs_down(self):
-        pass
+        query1 = {"name": name}
+        query2 = {"$inc": {"thumbs_up": 1}}
+        self.collection.update(query1, query2)
+
+    def thumbs_down(self, name):
+        query1 = {"name": name}
+        query2 = {"$inc": {"thumbs_down": 1}}
+        self.collection.update(query1, query2)
 
     def upload_video(self, name, theme):
-        pass
+
+        video = {
+
+            "name": name,
+            "theme": theme,
+            "thumbs_up": 0,
+            "thumbs_down": 0,
+            "score": 0
+        }
+        self.collection.insert(video)
+
+        return {"response": "video created"}
+
+
+db = Database()
+number = 0
 
 
 @view_config(route_name='home', renderer='templates/home_content.jinja2')
 def home(request):
 
-    db = Database()
-    videos = db.get_videos()
+    name = ""
 
+    if request.POST.get('name'):
+        name = str(request.POST.get('name'))
+
+        if request.POST.get('action') == "1":
+            print("Joinha")
+            db.thumbs_up(name)
+
+        else:
+            print("Não gostei")
+            db.thumbs_down(name)
+
+    videos = db.get_videos()
     return videos
 
 
 @view_config(route_name='themes', renderer='templates/themes_content.jinja2')
 def themes(request):
+
     return {}
 
 
@@ -48,4 +81,6 @@ def themes(request):
 def create(request):
     name = str(request.POST.get('name'))
     theme = str(request.POST.get('theme'))
-    return {"response": "video created"}
+    db.upload_video(name, theme)
+
+    return HTTPFound('/')
